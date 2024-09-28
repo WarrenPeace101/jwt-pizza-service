5ca5bec5f2eb48f1cd9b1f52f602d153504b0b1b
WarrenPeace101 <mangum.warren99@gmail.com>
2024-09-27 18:37:27 -0600

    added tests for login and logout fail

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -2,12 +2,15 @@ const request = require('supertest');
 const app = require('../service');
 
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
+//const testBadUser = {name: ''}
 let testUserAuthToken;
 
+//this function registers a new user before every other test
 beforeAll(async () => {
   testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
   const registerRes = await request(app).post('/api/auth').send(testUser);
   testUserAuthToken = registerRes.body.token;
+  testUserID = registerRes.body.user.id
 });
 
 test('login', async () => {
@@ -18,3 +21,53 @@ test('login', async () => {
   const { password, ...user } = { ...testUser, roles: [{ role: 'diner' }] };
   expect(loginRes.body.user).toMatchObject(user);
 });
+
+test('login fail', async () => {
+  testUser.email = null
+  const loginRes = await request(app).put('/api/auth').send(testUser);
+  expect(loginRes.status).toBe(404);
+
+})
+ 
+
+/*test('update user success', async () => {
+  //const testUserID = registerRes.body.user.id
+  //logs in user first
+ // const loginRes = await request(app).put('/api/auth').send(testUser);
+ // const loginUserID = loginRes.body.id
+  //const authToken = loginRes.body.token
+
+  const updateUserRes = await request(app).put(`/api/auth/:${testUserID}`).set('Authorization', `Bearer ${testUserAuthToken}`).send(testUser);
+  console.log(`/api/auth/:${testUserID}`)
+
+  expect(updateUserRes.status).toBe(200);
+
+});*/
+
+test('logout user success', async () => {
+  //login a user first
+  const loginRes = await request(app).put('/api/auth').send(testUser);
+
+  const deleteRes = await request(app).delete('/api/auth').set('Authorization', `Bearer ${testUserAuthToken}`).send(testUser);
+
+  expect(deleteRes.status).toBe(200)
+})
+
+test('logout user fail (no auth token)', async () => {
+  //login a user first
+  const loginRes = await request(app).put('/api/auth').send(testUser);
+
+  const deleteRes = await request(app).delete('/api/auth').send(testUser);
+
+  expect(deleteRes.status).toBe(401)
+})
+
+test('get pizza menu', async () => {
+
+  
+})
+
+
+
+
+
