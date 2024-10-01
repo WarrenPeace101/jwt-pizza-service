2a7230ef8a6a842382d2feacf67cfa0945ad9eec
WarrenPeace101 <mangum.warren99@gmail.com>
2024-10-01 08:41:52 -0600

    continued database tests

--- src/database/database.test.js
+++ src/database/database.test.js
@@ -1,15 +1,36 @@
 const request = require('supertest');
 const { DB, Role } = require('./database.js')
+const app = require('../service');
 
-//const app = require('../service');
-//const DB = require('../database.js')
+const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
+const orderSample = {franchiseId: 1, storeId:1, items:[{ menuId: 1, description: "Veggie", price: 0.05 }]}
+const newFranchise = {name: 'pizza pocket', admins: [{email: 'f@jwt.com'}]}
 
 
+if (process.env.VSCODE_INSPECTOR_OPTIONS) {
+    jest.setTimeout(60 * 1000 * 5); // 5 minutes
+  }
+
+function randomName() {
+    return Math.random().toString(36).substring(2, 12);
+  }
+  
+async function createAdminUser() {
+    let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
+    user.name = randomName();
+    user.email = user.name + '@admin.com';
+  
+    await DB.addUser(user);
+  
+    user.password = 'toomanysecrets';
+    return user;
+  }
 
 test('add menu item success', async () => {
     const myItem = {title: "hot dog", description: "yum", image: "x", price: "1.99"}
 
     const addMenuResult = DB.addMenuItem(myItem);
+    
 
     expect(addMenuResult).not.toBeNull();
 })
@@ -21,8 +42,75 @@ test('add user', async () => {
 })
 
 test('update user', async () => {
-    
+    const adminUser = createAdminUser()
+    //const loginRes = await request(app).put('/api/auth').send(adminUser);
+
+    //loginAuthToken = loginRes.body.token
+    //loginRes.body.id
+
+    const newUser = DB.updateUser(adminUser.id, adminUser.email, adminUser.password)
+
+    expect(newUser).not.toBeNull()
 })
 
-//test('')
+test('add diner order', async () => {
+const adminUser = createAdminUser()
+
+const addOrderRes = DB.addDinerOrder(adminUser, orderSample)
+
+expect(addOrderRes).not.toBeNull()
+
+})
+
+//to do
+test('get orders', async () => {
+    const adminUser = createAdminUser()
+
+    const orderList = DB.getOrders(adminUser)
+
+    expect(orderList).not.toBeNull()
+})
+
+test('insert new franchise', async () => {
+    const adminUser = createAdminUser()
+
+    const createFranchiseRes = DB.createFranchise(newFranchise)
+
+    expect(createFranchiseRes).not.toBeNull()
+})
+
+test('get a users franchise', async () => {
+
+    const adminUser = createAdminUser()
+
+    const getFranchiseRes = DB.getUserFranchises(adminUser.id)
+
+    expect(getFranchiseRes).not.toBeNull()
+})
+
+test('delete a franchise', async () => {
+    const adminUser = createAdminUser()
+
+    const deleteFranchRes = DB.deleteFranchise()
+
+    expect(deleteFranchRes.result).not.toBe(500)
+
+})
+
+test('get franchises admin user', async () => {
+    const adminUser = createAdminUser()
+
+    const getFranchRes = DB.getFranchises(adminUser)
+
+    expect(getFranchRes).not.toBeNull()
+})
+
+test('get franchises regular user', async () => {
+    const getFranchRes = DB.getFranchises(testUser)
+    expect(getFranchRes).not.toBeNull()
+})
+
+
+
+
 
--- src/routes/orderRouter.test.js
+++ src/routes/orderRouter.test.js
@@ -1,9 +1,25 @@
 const request = require('supertest');
 const app = require('../service');
+const {DB, Role} = require('../database/database.js')
 
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
 const menuItem = { title:"Student", description: "No topping, no sauce, just carbs", image:"pizza9.png", price: 0.0001};
 const orderSample = {franchiseId: 1, storeId:1, items:[{ menuId: 1, description: "Veggie", price: 0.05 }]}
+
+function randomName() {
+    return Math.random().toString(36).substring(2, 12);
+  }
+  
+async function createAdminUser() {
+    let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
+    user.name = randomName();
+    user.email = user.name + '@admin.com';
+  
+    await DB.addUser(user);
+  
+    user.password = 'toomanysecrets';
+    return user;
+  }
 
 //registers a new user before each test
 beforeAll(async () => {
@@ -19,7 +35,9 @@ test('get menu success', async () => {
 })
 
 test('add item to menu success', async () => {
-    const loginRes = await request(app).put('/api/auth').send(testUser);
+    const adminUser = createAdminUser()
+    const loginRes = await request(app).put('/api/auth').send(adminUser);
+
     loginAuthToken = loginRes.body.token
 
     const addItemRes = await request(app).put('/api/order/menu').set('Authorization', `Bearer ${loginAuthToken}`).send(menuItem);
@@ -50,12 +68,12 @@ test('get order for authenticated user fail (no auth token)', async () => {
 })
 
 test('create order for authenticated user', async () => {
-    const loginRes = await request(app).put('/api/auth').send(testUser);
+    const adminUser = createAdminUser()
+    const loginRes = await request(app).put('/api/auth').send(adminUser);
     loginAuthToken = loginRes.body.token
 
     const createOrderRes = await request(app).post('/api/order').set('Authorization', `Bearer ${loginAuthToken}`).send(orderSample);
     expect(createOrderRes.status).toBe(200);
-
 
 })
 
