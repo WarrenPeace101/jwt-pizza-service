c7ef0250771fed9da568e4885182a4afc38c6fa7
WarrenPeace101 <mangum.warren99@gmail.com>
2024-11-11 17:12:07 -0700

    started metrics, added metrics credentials

---
+++ src/metrics.js
@@ -0,0 +1,87 @@
+const config = require('./config.js');
+const os = require('os');
+
+class Metrics {
+  constructor() {
+    this.totalRequests = 0;
+    this.totalGetRequests = 0;
+    this.totalPostRequests = 0;
+    this.totalDeleteRequests = 0;
+
+    // This will periodically sent metrics to Grafana
+    const timer = setInterval(() => {
+      this.sendMetricToGrafana('request', 'all', 'total', this.totalRequests);
+    }, 10000);
+    timer.unref();
+  }
+
+  incrementRequests() {
+    this.totalRequests++;
+  }
+
+  sendMetricToGrafana(metricPrefix, httpMethod, metricName, metricValue) {
+    const metric = `${metricPrefix},source=${config.source},method=${httpMethod} ${metricName}=${metricValue}`;
+
+    fetch(`${config.url}`, {
+      method: 'post',
+      body: metric,
+      headers: { Authorization: `Bearer ${config.userId}:${config.apiKey}` },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          console.error('Failed to push metrics data to Grafana');
+        } else {
+          console.log(`Pushed ${metric}`);
+        }
+      })
+      .catch((error) => {
+        console.error('Error pushing metrics:', error);
+      });
+  }
+
+//   requestTracker((req, res, next) => {
+//     //sendMetricsPeriodically();
+//     //next();
+//     console.log();
+//   });
+
+  requestTracker() {
+    sendMetricsPeriodically();
+    next();
+  }
+
+  getCpuUsagePercentage() {
+    const cpuUsage = os.loadavg()[0] / os.cpus().length;
+    return cpuUsage.toFixed(2) * 100;
+  }
+  
+  getMemoryUsagePercentage() {
+    const totalMemory = os.totalmem();
+    const freeMemory = os.freemem();
+    const usedMemory = totalMemory - freeMemory;
+    const memoryUsage = (usedMemory / totalMemory) * 100;
+    return memoryUsage.toFixed(2);
+  }
+
+  sendMetricsPeriodically(period) {
+    const timer = setInterval(() => {
+      try {
+        const buf = new MetricBuilder();
+        httpMetrics(buf);
+        systemMetrics(buf);
+        userMetrics(buf);
+        purchaseMetrics(buf);
+        authMetrics(buf);
+  
+        const metrics = buf.toString('\n');
+        this.sendMetricToGrafana(metrics);
+      } catch (error) {
+        console.log('Error sending metrics', error);
+      }
+    }, period);
+  }
+}
+
+
+const metrics = new Metrics();
+module.exports = metrics;
